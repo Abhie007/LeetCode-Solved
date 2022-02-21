4c4529df0ba75870563702a95a1584b8cfdc8708
Abhie007 <95014782+Abhie007@users.noreply.github.com>
2022-02-21 20:41:15 +0530

    Time: 11 ms (71.61%), Space: 56.1 MB (23.87%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        // 
-        HashMap<String, List<String>> mp = new HashMap<>();
+        HashMap<String, List<String>> hm = new HashMap<>();
+        List<List<String>> list = new ArrayList<>(); //ans list
+        
         for(int i=0; i<strs.length; i++){
             String temp = strs[i];
-            char [] ch = temp.toCharArray();
-            Arrays.sort(ch);
-            if(mp.containsKey(String.valueOf(ch))){
-                mp.get(String.valueOf(ch)).add(strs[i]);
+            char[] c = temp.toCharArray();
+            Arrays.sort(c);
+            if(hm.containsKey(String.valueOf(c))){
+                hm.get(String.valueOf(c)).add(strs[i]);
             } else {
-                List<String> list = new ArrayList<>();
-                list.add(strs[i]);
-                mp.put(String.valueOf(ch), list);
+                List<String> lt = new ArrayList<>();
+                lt.add(strs[i]);
+                hm.put(String.valueOf(c), lt);
             }
         }
-        for(List<String> item: mp.values()){
-            res.add(item);
+        for( List<String> item : hm.values()){
+            list.add(item);
         }
-        return res;
-        
+        return list;
     }
 }
