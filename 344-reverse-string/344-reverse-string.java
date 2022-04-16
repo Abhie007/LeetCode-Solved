d8bcef291a141a97f900624e04a7e7c03be41acb
Abhie007 <95014782+Abhie007@users.noreply.github.com>
2022-04-16 14:31:48 +0530

    Time: 1 ms (81.73%), Space: 49 MB (93.50%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -4,10 +4,10 @@ class Solution {
         int j = s.length-1;
         while(i<j){
             char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            i++;
-            j--;
+            s[i++] = s[j];
+            s[j--] = temp;
+            // i++;
+            // j--;
         }
     }
 }
