96dc74c234a9cf27c59835fd844d065f57f21b9b
Abhie007 <95014782+Abhie007@users.noreply.github.com>
2022-05-01 09:03:50 +0530

    Added solution - LeetHub

--- Three way partitioning - GFG/three-way-partitioning.java
+++ Three way partitioning - GFG/three-way-partitioning.java
@@ -105,24 +105,6 @@ class Solution{
         if(array.length==0){
     return;
     }
-    int start=0;
-    int end=array.length-1;
-    for(int i=0;i<=end;){
-    if(array[i]<a){
-    int temp=array[start];
-    array[start]=array[i];
-    array[i]=temp;
-    start++;
-    i++;
-    }
-    else if(array[i]>b){
-    int temp=array[end];
-    array[end]=array[i];
-    array[i]=temp;
-    end--;
-    }
-    else
-    i++;
-    }
+    Arrays.sort(array);
     }
 }
